cb436dd7890bf0c592ff316bcb12c7d732a37d00
JonyPlo <joonyyplo@gmail.com>
2023-11-29 04:46:00 -0300

    feat: :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const getRandomNumberFromApi = async (): Promise<number> => {
+  const res = await fetch(
+    'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+  )
+  const numberString = await res.text()
+  return +numberString
+}
+
+export const App = () => {
+  const [number, setNumber] = useState<number>()
+
+  useEffect(() => {
+    getRandomNumberFromApi().then((num) => setNumber(num))
+  }, [])
+
+  return (
+    <div className='App-header'>
+      <h2>Numero aleatorio: {number}</h2>
+    </div>
+  )
+}
